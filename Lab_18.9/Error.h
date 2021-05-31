eb562a80457f21952008763d1de1ad3b845e60bb
KamenevVA <73518278+KamenevVA@users.noreply.github.com>
2021-05-31 17:25:12 +0500

    Create Error.h

---
+++ Lab_18.9/Error.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+class error
+{
+	string str;
+public:
+
+	error(string s) { str = s; }
+	void what() { cout << str << endl; } 
+};
